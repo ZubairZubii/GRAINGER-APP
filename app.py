a91661d2d1a24dc87d67ade34c62d1fb69268aae
ZubairZubii <zs970120@gmail.com>
2025-10-23 15:00:33 +0500

    deploy

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def scrape_endpoint():
                 driver.quit()
             except:
                 pass
-
+ #commit
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
